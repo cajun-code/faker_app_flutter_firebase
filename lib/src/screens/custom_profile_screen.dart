2363d486ab8a6e66b7375e5c7e0b3c12d39e944c
Allan Davis <cajun.code@gmail.com>
2023-11-20 21:31:40 -0600

    cleaned up profile

--- lib/src/screens/custom_profile_screen.dart
+++ lib/src/screens/custom_profile_screen.dart
@@ -1,4 +1,3 @@
-import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:faker_app_flutter_firebase/src/screens/ui_auth_providers.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
